84a6c62adaf893b4ef212c21682d2a25f3e71d3b
Pranjal Kaushik <prnjk31100@gmail.com>
2021-05-06 12:31:25 +0530

    list of government website for covid information

---
+++ src/App.js
@@ -0,0 +1,240 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="homepg absolute-center">
+        <h1 className="heading1"> Sites for Corona Virus Update </h1>
+      </div>
+
+      {/* <hr></hr> */}
+
+      <section className="section1class">
+        <div className="section1div">
+          <div className="section2div">
+            <h2 className="heading2">Site for Hospital beds availability</h2>
+            <ul>
+              <li className="designli">
+                <a href="https://covidinfo.rajasthan.gov.in/Covid-19hospital-wisebedposition-wholeRajasthan.aspx">
+                  <h4> Rajasthan </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="http://corona.delhi.gov.in/">
+                  <h4>Delhi</h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="http://dgmhup.gov.in/en/default">
+                  <h4> UttarPradesh Site1</h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://rahat.up.nic.in/covid19dashboard.aspx">
+                  <h4> UttarPradesh Site 2 (Rahat)</h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="http://sarthak.nhmmp.gov.in/covid/">
+                  <h4> Madhya Pradesh Site1 </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://mapit.gov.in/covid-19/">
+                  <h4> Madhya Pradesh Site 2 </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://www.jharkhand.gov.in/Home/Covid19Dashboard">
+                  <h4> Jharkhand </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://covid19.assam.gov.in/">
+                  <h4> Assam </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="http://statehealthsocietybihar.org/corona/">
+                  <h4> Bihar </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="http://chdcovid19.in/">
+                  <h4> Chandigarh </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://cg.nic.in/health/covid19/RTPBedAvailable.aspx">
+                  <h4> Chhattisgarh </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://covidfacility.dddgov.in/">
+                  <h4> DADRA AND NAGAR HAVELI AND DAMAN AND DIU </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://coronabeds.jantasamvad.org/">
+                  <h4> New Delhi </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://covid.ladakh.gov.in/">
+                  <h4> Ladakh </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://www.goa.gov.in/covid-19/">
+                  <h4> Goa </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://gujcovid19.gujarat.gov.in/">
+                  <h4> Gujarat </h4>
+                </a>
+                <a href="https://ahna.org.in/covid19.html">
+                  <h4>Ahmedabad</h4>
+                </a>
+                <a href="https://vmc.gov.in/Covid19VadodaraApp/Default.aspx">
+                  <h4>Vadodara</h4>
+                </a>
+                <a href="http://office.suratsmartcity.com/SuratCOVID19/">
+                  <h4>Surat</h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://coronaharyana.in/">
+                  <h4> Haryana </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://covidggn.com/public/pages/gurugram-hospitals">
+                  <h4> Gurugram </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://covid19jagratha.kerala.nic.in/home/addHospitalDashBoard">
+                  <h4> Kerala </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://nmmchealthfacilities.com/HospitalInfo/showhospitalist">
+                  <h4> Navi Mumbai </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://www.divcommpunecovid.com/ccsbeddashboard/hsr">
+                  <h4> Pune </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://covidthane.org/availabiltyOfHospitalBeds.html">
+                  <h4> Thane </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="http://nsscdcl.org/covidbeds/AvailableHospitals.jsp">
+                  <h4> Nagpur </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="http://covidcbrs.nmc.gov.in/home/hospitalSummary">
+                  <h4> Nashik </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://meghealth.in/MeghCare.html">
+                  <h4> Meghalaya </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://statedashboard.odisha.gov.in/">
+                  <h4> Odisha </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://covid19dashboard.py.gov.in/">
+                  <h4> Puducherry </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://www.covid19sikkim.org/">
+                  <h4> Sikkim </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://stopcorona.tn.gov.in/">
+                  <h4> Tamil Nadu </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://covid19.telangana.gov.in/">
+                  <h4> Telangana </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://covid19.tripura.gov.in/">
+                  <h4> Tripura </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://covid19.uk.gov.in/bedssummary.aspx">
+                  <h4> Uttarakhand </h4>
+                </a>
+              </li>
+
+              <li className="designli">
+                <a href="https://www.wbhealth.gov.in/">
+                  <h4> West Bengal </h4>
+                </a>
+              </li>
+
+              {/* <li className="designli">
+                <a href="">
+                  <h4> State Name </h4>
+                </a>
+              </li> */}
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+// npm install -g firebase-tools
+
+export default App;
